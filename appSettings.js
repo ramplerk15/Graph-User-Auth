4c692a40bba552cfea5eca8db48e4ef177c4965b
kemara_23 <marina.rampler@gmail.com>
2024-05-16 01:13:36 +0200

    auth token & inbox

---
+++ appSettings.js
@@ -0,0 +1,11 @@
+const settings = {
+    'clientId': '2e916c8f-488f-4da1-98f7-b4e5cc1900dd',
+    'tenantId': 'c5f0fc93-918e-4456-8a03-a1e68cb4b38c',
+    'graphUserScopes': [
+      'user.read',
+      'mail.read',
+      'mail.send'
+    ]
+  };
+  
+  module.exports = settings;
